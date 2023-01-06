35b92bb2762d15622f02eb50c285af18be44d5fd
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-01-06 01:39:06 -0700

    converted pages to flask, added image from Figma to 404 page

--- main.py
+++ main.py
@@ -13,8 +13,21 @@ def homepage():
 
     return render_template('home.html')
 
+@app.route('/poll-results')
+def poll_results():
+    """A page to display the poll results"""
+
+    return render_template('poll-results.html')
+
+@app.route('/sketches')
+def sketches():
+    """A page about the details of Paul Doerr's sketches"""
+
+    return render_template('sketches.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
+    """A error page for this site"""
     return render_template("404-error-page.html")
 
 if __name__ == '__main__':
