1f451cb70bcfa5c528e5cbadaaa0a1f590e052bb
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-01-09 22:40:06 -0700

    restored poll to homepage, cleaned up files

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # 2023-winter-intensive
- Continuation of Zodiac Deciphered
+ Continuation of Deciphered (now Zodiac Deciphered)
  
  [previous repo](https://github.com/paloma-vm/capstone-bootstrap-website.git)
+
+ [wireframe](intensive_files/winter intensive wireframe.png)
+
+[Figma workbook](https://www.figma.com/file/HS47eUp8k3dbfebma3MniA/winter-intensive-wireframe?node-id=0%3A1&t=ClIo1fYsUYnOEfVd-1)
+
+I used masking to cut out Zodiac characters to make a custom 404 page graphic.
+
+[screenshots](intensive_files)
+
+The first main blocker I encountered was broken image links.  I did research and found out how to make a static folder and then changed all the links.
+
+I needed help from Mitchell to get the flask poll running and connected to the database.
+
+I did not get to the point of deployment.  I will have to learn how to deploy a flask app.
+
+After the pitch, I removed the /poll page and restored the poll to the homepage.
+
+I learned a lot and did a lot of research during this Intensive.  I was inspired by Kash's workshop to do data visualization in the future.
+
--- removed-items/main copy 2.py
+++ removed-items/main copy 2.py
@@ -11,17 +11,16 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/zodiacPollAnswers"
 mongo = PyMongo(app)
 
-# @app.route('/')
-# def homepage():
-#     """A homepage for this site"""
-    
-#     return render_template('home.html')
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def homepage():
     """A homepage for this site"""
-    # ---------------------------------
-    # POLL section
+    
+    return render_template('home.html')
+
+@app.route('/poll', methods=['GET', 'POST'])
+def update_poll():
+    """A poll on this site"""
+    print('/poll *****************')
     if request.method == 'POST':
         # get user's answer and store it in the object new_poll_answer
         new_poll_answer = {
@@ -30,10 +29,12 @@ def homepage():
         print(request.form.get('user_answer'))
         # insert object into answers database
         result = mongo.db.answers.insert_one(new_poll_answer)
+        print('___________________')
+        print(new_poll_answer)
 
         return redirect(url_for('poll_results'))#name of the function
     else:
-        return render_template('home.html')
+        return render_template('poll.html')
 
 @app.route('/poll-results', methods=['GET', 'POST'])
 def poll_results():
@@ -42,33 +43,48 @@ def poll_results():
   
     poll_data = mongo.db.answers.find() #do I need this?  I don't think so.
     
+
+   
     # find all yes answers
     yes_answers = mongo.db.answers.find({'users_answer': 'yes'})
 
+ 
     # count the yes answers
     yes_answers_count = len(list(yes_answers))
+
+    print('~~~~~~~~~~~~~~~~')
+    print(yes_answers_count)
 
     # find all no answers
     no_answers = mongo.db.answers.find({'users_answer': 'no'})
     # count the no answers
     no_answers_count = len(list(no_answers))
-    # find all maybe answers
+    print('~~~~~~~~~~~~~~~~')
+    print(no_answers_count)
+     # find all maybe answers
     maybe_answers = mongo.db.answers.find({'users_answer': 'maybe'})
     # count the maybe answers
     maybe_answers_count = len(list(maybe_answers))
+    print('~~~~~~~~~~~~~~~~')
+    print(maybe_answers_count)
 
     context = {
         'yes_answers_count': yes_answers_count,
         'no_answers_count': no_answers_count,
         'maybe_answers_count': maybe_answers_count,
+
     }
 
+
     return render_template('poll-results.html', **context)
+
+
 
 
 @app.route('/sketches')
 def sketches():
     """A page about the details of Paul Doerr's sketches"""
+
     return render_template('sketches.html')
 
 @app.errorhandler(404)
